b82e69443f901f7510e55ee19b30226bbad34a8e
zureeofficial <zuree1625@gmail.com>
2025-09-30 07:45:20 -0700

    Full working Zuree site

--- pages/admin.js
+++
@@ -1,8 +0,0 @@
-export default function Admin() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Admin Panel</h1>
-      <p>Login required to access this page.</p>
-    </div>
-  );
-}
