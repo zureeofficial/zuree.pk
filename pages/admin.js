c68d166e4439e81a38dc97bb9c611d8df995df06
zureeofficial <zuree1625@gmail.com>
2025-09-30 04:15:54 -0700

    First upload of Zuree.pk site

---
+++ pages/admin.js
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Admin Panel</h1>
+      <p>Login required to access this page.</p>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Zuree.pk</h1>
+      <p>Beautiful baby clothing, just for you.</p>
+    </div>
+  );
+}
